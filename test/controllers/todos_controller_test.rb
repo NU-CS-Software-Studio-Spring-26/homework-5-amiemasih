89ad04f5811b0d67ef24af9a224432e3d4f30b44
amiemasih <amiemasih2027@u.northwestern.edu>
2026-05-30 20:10:35 -0500

    Test toggle_priority returns a Turbo Stream and flips high_priority

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -45,4 +45,11 @@ class TodosControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to todos_url
   end
+
+  test "toggle_priority responds with turbo stream and flips high_priority" do
+    todo = todos(:one)
+    patch toggle_priority_todo_url(todo), headers: { "Accept" => "text/vnd.turbo-stream.html" }
+    assert_equal "text/vnd.turbo-stream.html", response.media_type
+    assert_equal true, todo.reload.high_priority
+  end
 end
